7993624c400a25dc5e06a6998621335459da52e7
shravanthi-mikky <shravanthi27041996@gmail.com>
2023-01-11 08:15:01 +0530

    [shravanthi] - Welcome msg

---
+++ ArthemeticOperations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArthemeticOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Arthemetic Operations");
+        }
+    }
+}
